a325333c283f77366213f9d732fdc1fdadd8d576
lmascelli <lmascelli@gmail.com>
2022-12-15 15:25:55 +0100

    pwsh progeres on emacs

--- nvim/config.lua
+++ nvim/config.lua
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-local config = {}
-
-config.colorscheme = 'default'
-config.font_name = 'FiraCode NF'
-config.font_size = 10
-
-config.opts = {
-
-}
-
-config.settings = {
-  colorcolumn = 80,
-}
-
-config.plugin_list = {
-  'which-key',
-  'nvim-tree',
-  'telescope',
-  'lualine',
-  'bufferline',
-  'lsp',
-  'lspsaga',
-  'nvim-cmp',
-  'luasnip',
-  'comment',
-  'nvim-autopairs',
-  'toggleterm',
-  'treesitter',
-  'startup-time',
-  'black',
-}
-
-
-config.lsp_list = {
-  'lua',
-  'clangd',
---  'pyright',
-  'pylsp',
-  'rust_analyzer',
-  'powershell',
-}
-
-config.matlab_exec = 'D:/Program Files/MATLAB/R2022b/bin/matlab'
-
-return config
-=======
 local config = {}
 
 config.colorscheme = 'default'
@@ -93,4 +46,3 @@ config.lsp_list = {
 }
 
 return config
->>>>>>> 1f6cd88f7581e8de3631f69fc52c888c2e86ed29
