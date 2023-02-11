1598f06c5c4e54f468cfae4a57227c4a41c49e2b
lmascelli <lmascelli@gmail.com>
2023-02-11 09:00:24 +0100

    gh_swap fixed to use XDG_CONFIG_HOME env

--- scripts/gh_swap.ps1
+++ scripts/gh_swap.ps1
@@ -3,9 +3,9 @@ switch ($args[0]) {
 		gh auth status
 	    }
 	    "swap" {
-		Copy-Item -Path ~/.config/gh/hosts.yml -Destination ~/.config/gh/_tmp_hosts.yml
-		Copy-Item -Path ~/.config/gh/other_hosts.yml -Destination ~/.config/gh/hosts.yml
-		Copy-Item -Path ~/.config/gh/_tmp_hosts.yml -Destination ~/.config/gh/other_hosts.yml
+		Copy-Item -Path $env:XDG_CONFIG_HOME/gh/hosts.yml -Destination $env:XDG_CONFIG_HOME/gh/_tmp_hosts.yml
+		Copy-Item -Path $env:XDG_CONFIG_HOME/gh/other_hosts.yml -Destination $env:XDG_CONFIG_HOME/gh/hosts.yml
+		Copy-Item -Path $env:XDG_CONFIG_HOME/gh/_tmp_hosts.yml -Destination $env:XDG_CONFIG_HOME/gh/other_hosts.yml
 	    }
 	    default {
 		Write-Output "use [status] or [swap]"
