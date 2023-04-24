9a86c799e03972dd23558f7719a11abb6cc3ccdc
lmascelli <lmascelli@gmail.com>
2023-04-24 17:19:56 +0200

    init dot_update.ps1

--- scripts/dot_update.ps1
+++ scripts/dot_update.ps1
@@ -1,111 +1,25 @@
-# Installation an update of the system in an unique file. WOW
+################################################################################
+#                                                                              #
+#                       WINDOWS INSTALLATION SCRIPT                            #
+#                                                                              #
+################################################################################
 
-#-------------------------------------------------------------------------------
-#
-#                            GLOBAL VARIABLES
-#
-#-------------------------------------------------------------------------------
+# Global variables
+$scoop_dir = 'D:\Scoop'
 
-$LM_Config_File_Path = "~/lmconfig"
+# Install scoop
 
-$global:Glob = @{
-    InstallPath = $null
-    ConfigPath = $null
-    BinPath = $null
-}
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+irm get.scoop.sh -outfile 'install.ps1'
+.\install.ps1 -ScoopDir 'D:\Scoop' -ScoopGlobalDir 'D:\Scoop' -NoProxy
 
-
-#-------------------------------------------------------------------------------
-#
-#                            UTILITY FUNCTIONS
-#
-#-------------------------------------------------------------------------------
-
-Function LM_Read {
-    param (
-	$Prompt
-    )
-    $ret = Read-Host -Prompt $Prompt
-    if ($ret -eq "") {
-	return $null
-    } else {
-	return $ret
-    }
-}
-
-
-#-------------------------------------------------------------------------------
-#
-#                            CONFIGURATION OBJECT
-#
-#-------------------------------------------------------------------------------
-
-Function LM_Init_Glob {
-    $global:Glob.InstallPath = LM_read -Prompt "Insert folder where to install dotfiles"    
-    $global:Glob.ConfigPath = LM_read -Prompt "Insert folder where to save config"
-    $global:Glob.BinPath = LM_read -Prompt "Insert folder where to store bin"
-}
-
-Function LM_Save_Glob {
-    ConvertTo-Json $global:Glob | Set-Content -Path $LM_Config_File_Path -Force
-}
-
-Function LM_Read_Glob {
-    $global:Glob = (Get-Content -Path $LM_Config_File_Path | ConvertFrom-Json )
-}
-
-
-#-------------------------------------------------------------------------------
-#
-#                              PACKAGE MANAGER
-#
-#-------------------------------------------------------------------------------
-
-
-#-------------------------------------------------------------------------------
-#
-#                                 WEZTERM
-#
-#-------------------------------------------------------------------------------
-
-
-Function Update-Wezterm {
-    scoop update wezterm
-
-    Copy-Item -Force -Path $global:Glob.InstallPath/wezterm -Destination $global:Glob.ConfigPath -Recurse
-}
-
-
-#-------------------------------------------------------------------------------
-#
-#                                 SCRIPTS
-#
-#-------------------------------------------------------------------------------
-
-
-Function Update-Scripts {
-    $path = 
-    Copy-Item -Force -Path ($global:Glob.InstallPath + "/scripts/*") -Destination $global:Glob.BinPath -Recurse
-}
-
-
-#-------------------------------------------------------------------------------
-#
-#                                 MAIN
-#
-#-------------------------------------------------------------------------------
-
-if (Test-Path $LM_Config_File_Path) { 
-    LM_Read_Glob
-    Write-Output $Glob
-} else {
-    LM_Init_Glob
-    LM_Save_Glob
-}
-
-if ($isWindows) {
-    #    [Console]::Out.Flush()
-    Update-Scripts
-} elseif ($isLinux) {
-    
-}
+scoop install 7zip 
+scoop install git
+scoop install msys2
+scoop install wezterm
+scoop install pwsh
+scoop install miniconda3
+scoop held msys2
+scoop bucket add kiennq-scoop
+scoop bucket add nerd-fonts
+scoop install emacs-k
