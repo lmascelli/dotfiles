f8f31d5fe34cdf39074e926c13316f3f3dd88015
lmascelli <lmascelli@gmail.com>
2023-06-10 10:35:35 +0200

    node installer

--- scripts/installers/node.ps1
+++ scripts/installers/node.ps1
@@ -3,7 +3,7 @@ if ($IsLinux) {
   $node_version = "18.16.0"
   Invoke-WebRequest -URI "https://nodejs.org/dist/v$($node_version)/node-v$($node_version)-linux-x64.tar.xz" -OutFile ~/.lm/download/node.tar.xz
   tar -xf ~/.lm/download/node.tar.xz
-  ln -s "~/.lm/node-v$($node_version)-linux-x64/bin/node" ~/.local/bin/node
-  ln -s "~/.lm/node-v$($node_version)-linux-x64/bin/npm" ~/.local/bin/npm
+  New-Item -ItemType SymbolicLink -Path "~/.lm/node-v$($node_version)-linux-x64/bin/node" -Target "~/.local/bin/node"
+  New-Item -ItemType SymbolicLink -Path "~/.lm/node-v$($node_version)-linux-x64/bin/npm" -Target "~/.local/bin/npm"
   Pop-Location
 }
