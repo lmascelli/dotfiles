de7efbdce48c4ba4a0980ff2142028d8831b73e4
lmascelli <lmascelli@gmail.com>
2023-06-21 17:00:49 +0200

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,8 @@ echo
 echo "Installing pwsh ..."
 bash "./scripts/installers/pwsh.sh"
 
+echo "Installing neovim..."
 pwsh -c "./scripts/installers/neovim.ps1"
+
+echo "Installing start script"
+cp "./scripts/start.sh" ~
