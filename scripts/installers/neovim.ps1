fe96c9d6537fd21cde15e3e40ae42a4df32dbee4
lmascelli <lmascelli@gmail.com>
2023-06-21 17:23:24 +0200

    bootstrap

--- scripts/installers/neovim.ps1
+++ scripts/installers/neovim.ps1
@@ -2,6 +2,7 @@ if ($IsLinux) {
   Push-Location ~/.lm
   Invoke-WebRequest -URI "https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz" -OutFile ~/.lm/download/neovim.tar.gz
   tar -xf ~/.lm/download/neovim.tar.gz
+  New-Item -ItemType SymbolicLink -Path "~/.lm/nvim-linux64/bin/nvim" -Value "~/.local/bin/nvim"
   New-Item -ItemType SymbolicLink -Path "~/.config/nvim" -Value "~/.dotfiles/nvim"
   Pop-Location
 }
