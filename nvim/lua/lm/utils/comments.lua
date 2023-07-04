64a5dd06f8d36ad2b7a7949ee4e4f754e62a3366
lmascelli <lmascelli@gmail.com>
2023-07-04 15:05:21 +0200

    insert comment template positioning

--- nvim/lua/lm/utils/comments.lua
+++ nvim/lua/lm/utils/comments.lua
@@ -1,6 +1,8 @@
 LM.comments = {
   template = "",
   insert = function()
-    vim.fn.append(vim.fn.getpos('.')[2] - 1, LM.comments.template)
+    local cur_pos = vim.fn.getpos('.')[2]
+    vim.fn.append(cur_pos - 1, LM.comments.template)
+    vim.fn.cursor(cur_pos + 1, 3)
   end
 }
