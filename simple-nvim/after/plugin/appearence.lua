cc269e93e93a617fa0be701d5d76bf295d3285dd
lmascelli <lmascelli@gmail.com>
2023-08-16 09:08:45 +0200

    nvim default colorscheme tweaks in appearence.lua

--- simple-nvim/after/plugin/appearence.lua
+++ simple-nvim/after/plugin/appearence.lua
@@ -5,7 +5,14 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   callback = function()
     vim.cmd "highlight Comment cterm=italic gui=italic"
     vim.cmd "highlight WinSeparator guibg=None"
+    if vim.g.colors_name == 'default' then
+      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = '#3B4252', fg = '#5E81AC' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#3B4252' })
+      vim.api.nvim_set_hl(0, 'PMenu', { bg = '#3B4252', fg = '#5E81AC' })
+      vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = '#3B4252' })
+      vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = '#3B4252' })
+    end
   end,
 })
 
-vim.cmd ('colorscheme ' .. 'slate')
+vim.cmd('colorscheme ' .. 'slate')
