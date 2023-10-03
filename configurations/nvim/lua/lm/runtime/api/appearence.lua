f8acc45fdce1a99020834ce6ecc7819808dead6a
lmascelli <lmascelli@gmail.com>
2023-10-03 21:16:51 +0200

    move toggle transparency to LM.api.appearence

--- configurations/nvim/lua/lm/runtime/api/appearence.lua
+++ configurations/nvim/lua/lm/runtime/api/appearence.lua
@@ -1,7 +1,7 @@
 LM.api.appearence = {
-  load_theme = function ()
-     local key = vim.api.nvim_replace_termcodes("<escape>:colorscheme ", true, false, true)
-     vim.api.nvim_feedkeys(key, 'n', false)
+  load_theme = function()
+    local key = vim.api.nvim_replace_termcodes("<escape>:colorscheme ", true, false, true)
+    vim.api.nvim_feedkeys(key, 'n', false)
   end,
 
   -- toggle the colorcolumn at line 80
@@ -13,4 +13,14 @@ LM.api.appearence = {
       vim.opt.colorcolumn = tostring()
     end
   end,
+  toggle_transparency = function()
+    if LM.appearence.transparency then
+      vim.cmd('colorscheme ' .. LM.appearence.colorscheme)
+      LM.appearence.transparency = false
+    else
+      vim.cmd 'hi Normal guibg=NONE ctermbg=NONE'
+      vim.cmd 'hi EndOfBuffer guibg=NONE ctermbg=NONE'
+      LM.appearence.transparency = true
+    end
+  end
 }
--- configurations/nvim/lua/lm/runtime/init.lua
+++ configurations/nvim/lua/lm/runtime/init.lua
@@ -17,16 +17,6 @@ LM = {
   appearence = {
     colorscheme = 'default',
     transparency = false,
-    toggle_transparency = function()
-      if LM.appearence.transparency then
-        vim.cmd('colorscheme ' .. LM.appearence.colorscheme)
-        LM.appearence.transparency = false
-      else
-        vim.cmd 'hi Normal guibg=NONE ctermbg=NONE'
-        vim.cmd 'hi EndOfBuffer guibg=NONE ctermbg=NONE'
-        LM.appearence.transparency = true
-      end
-    end
   },
   api = {
     complete = function()
