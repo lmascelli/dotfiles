605d9678c2ae31d04e45c713f34b0115a1496cf8
lmascelli <40148338+lmascelli@users.noreply.github.com>
2023-10-28 20:41:25 +0200

    Create custom.lua

---
+++ configurations/nvim/custom.lua
@@ -0,0 +1,12 @@
+-- colorscheme
+LM.appearence.colorscheme = 'default'
+
+-- font
+LM.font.font_name = ''
+LM.font.font_size = 10
+
+-- orgmode
+LM.orgmode_emacs_config = {
+  executable_path = '',
+  config_path = '',
+}
