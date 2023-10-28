c8ea9cbd845800d7cc1cfb89c503548af3906858
lmascelli <40148338+lmascelli@users.noreply.github.com>
2023-10-28 20:34:24 +0200

    merge

--- configurations/nvim/custom.lua
+++ configurations/nvim/custom.lua
@@ -2,14 +2,14 @@
 -- custom configuration file. this file doesn't get updated
 -- machine dependent configuration goes here
 
-LM.appearence.colorscheme = 'lunaperche'
-LM.font.font_name = 'Cascadia Code'
-LM.font.font_size = 11
+LM.appearence.colorscheme = 'default'
+LM.font.font_name = ''
+LM.font.font_size = 10
 
 -- plugins related
 
 -- orgmode
 LM.orgmode_emacs_config = {
-  executable_path = 'D:\\Program Files\\emacs-29.1_2\\bin\\emacs.exe',
-  -- config_path = '$HOME/AppData/Roeaming/.emacs.d/init.el'
+  executable_path = 'emacs',
+  config_path = '$HOME/.config/emacs/init.el'
 }
