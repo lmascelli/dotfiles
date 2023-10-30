10c3dfa75f9f117ffcb28919d61cb913cd6c714c
lmascelli <lmascelli@gmail.com>
2023-10-30 23:59:38 +0100

    mason-lspconfig automatic_install

--- configurations/nvim/lua/lm/plugins/lsp.lua
+++ configurations/nvim/lua/lm/plugins/lsp.lua
@@ -23,61 +23,63 @@ return {
     lazy = true,
     config = function()
       require('mason').setup()
-      require('mason-lspconfig').setup()
+      require('mason-lspconfig').setup({
+        automatic_installation = true,
+      })
 
-    local lspconfig = require('lspconfig')
+      local lspconfig = require('lspconfig')
 
-    local setup_server = function(server, opts)
-      opts = opts or {}
-      opts.on_attach = LM.lsp.on_attach.default
-      lspconfig[server].setup(opts)
-    end
+      local setup_server = function(server, opts)
+        opts = opts or {}
+        opts.on_attach = LM.lsp.on_attach.default
+        lspconfig[server].setup(opts)
+      end
 
-    setup_server('lua_ls', {
-      settings = {
-        Lua = {
-          runtime = {
-            version = 'LuaJIT',
-            path = vim.split(package.path, ';'),
-          },
-          diagnostics = {
-            globals = { 'vim', 'LM', },
-          },
-          workspace = {
-            library = {
-              [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-              [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+      setup_server('lua_ls', {
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+              path = vim.split(package.path, ';'),
             },
-          },
-        }
-      },
-    })
-    setup_server('clangd', {
-      cmd = {
-        "clangd",
-        "--background-index",
-        "-j=12",
-        "--query-driver=" .. LM.lsp.c_query_driver,
-        "--clang-tidy",
-        "--clang-tidy-checks=*",
-        "--all-scopes-completion",
-        "--cross-file-rename",
-        "--completion-style=detailed",
-        "--header-insertion-decorators",
-        "--header-insertion=iwyu",
-        "--pch-storage=memory",
-      },
-      capabilities = LM.lsp.capabilities,
-      init_options = {
-        clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
-        usePlaceholders = true,
-        completeUnimported = true,
-        semanticHighlighting = true,
-      },
-    })
-    setup_server('pylsp')
-    -- to install also mypy run ':PylspInstall pyls-flake8 pylsp-mypy pyls-isort'
-    setup_server('powershell_es')
-  end
-}
+            diagnostics = {
+              globals = { 'vim', 'LM', },
+            },
+            workspace = {
+              library = {
+                [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+              },
+            },
+          }
+        },
+      })
+      setup_server('clangd', {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "-j=12",
+          "--query-driver=" .. LM.lsp.c_query_driver,
+          "--clang-tidy",
+          "--clang-tidy-checks=*",
+          "--all-scopes-completion",
+          "--cross-file-rename",
+          "--completion-style=detailed",
+          "--header-insertion-decorators",
+          "--header-insertion=iwyu",
+          "--pch-storage=memory",
+        },
+        capabilities = LM.lsp.capabilities,
+        init_options = {
+          clangdFileStatus = true, -- Provides information about activity on clangd’s per-file worker thread
+          usePlaceholders = true,
+          completeUnimported = true,
+          semanticHighlighting = true,
+        },
+      })
+      setup_server('pylsp')
+      -- to install also mypy run ':PylspInstall pyls-flake8 pylsp-mypy pyls-isort'
+      setup_server('powershell_es')
+    end
+  }
 }
