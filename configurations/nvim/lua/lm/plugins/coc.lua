823f530ceb8888846b961309de741cf55162ec7d
lmascelli <lmascelli@gmail.com>
2023-12-07 08:09:03 +0100

    coc keymaps

--- configurations/nvim/lua/lm/plugins/coc.lua
+++ configurations/nvim/lua/lm/plugins/coc.lua
@@ -17,34 +17,79 @@ return {
       end
     end
     -- vim.g.coc_start_at_startup = false;
+    vim.keymap.set('n', '<leader>ll', '<cmd>lua LM.coc_toggle()<cr>', { desc = "enable" })
+  end,
+  config = function()
     vim.keymap.set('n', '<leader>ff', '<cmd>CocList files<cr>', { desc = "enable" })
     vim.keymap.set('n', '<leader>fg', '<cmd>CocList grep<cr>', { desc = "enable" })
     vim.keymap.set('n', '<leader>bl', '<cmd>CocList buffers<cr>', { desc = "enable" })
-    vim.keymap.set('n', '<leader>ll', '<cmd>lua LM.coc_toggle()<cr>', { desc = "enable" })
     vim.keymap.set('i', '<c-x><c-o>', 'coc#refresh()', { silent = true, expr = true })
-    vim.keymap.set('n', '<leader>lK', '<cmd>eval CocActionAsync("doHover")<cr>', { desc = "documentation" })
+    vim.keymap.set('n', '<leader>lk', '<cmd>lua _G.show_docs()<cr>', { desc = "documentation", silent = true })
     -- <space>gs --> Signature
-    vim.keymap.set('n', '<leader>lk', '<cmd>eval CocActionAsync("showSignatureHelp")<cr>', { desc = "signature" })
+    vim.keymap.set('n', '<leader>ls', '<cmd>eval CocActionAsync("showSignatureHelp")<cr>', { desc = "signature" })
     -- <space>gf --> Format document
-    vim.keymap.set('n', '<leader>lf', '<cmd>eval CocActionAsync("format")<cr>', { desc = "format" })
+    vim.keymap.set('n', '<leader>lf', '<cmd>eval CocActionAsync("format")<cr>', { desc = "format", silent = true })
     --    -- <space>gr --> Refactor
-    vim.keymap.set('n', '<leader>lcr', '<Plug>(coc-rename)', { desc = "refactor" })
+    vim.keymap.set('n', '<leader>lr', '<Plug>(coc-rename)', { desc = "rename", silent = true })
     -- <space>ge --> Definition
     vim.keymap.set('n', '<leader>lgd', '<Plug>(coc-definition)', { desc = "definition" })
+    vim.keymap.set('n', '<leader>lgy', '<Plug>(coc-type-definition)', { desc = "type definition" })
+    vim.keymap.set('n', '<leader>lgi', '<Plug>(coc-implementation)', { desc = "implementation" })
+    vim.keymap.set('n', '<leader>lgr', '<Plug>(coc-reference)', { desc = "reference" })
     -- <space>ge --> Diagnostics
-    vim.keymap.set('n', '<leader>ld', '<cmd>CocList diagnostics<cr>', { desc = "diagnostics" })
+    vim.keymap.set('n', '<leader>ld', '<cmd>CocDiagnostics<cr>', { desc = "diagnostics" })
+    vim.keymap.set('n', '<leader>lgp', '<Plug>(coc-diagnostic-prev)', { desc = "previous error" })
+    vim.keymap.set('n', '<leader>lgn', '<Plug>(coc-diagnostic-next)', { desc = "next error" })
     -- <space>g. --> Fix
-    vim.keymap.set('n', '<leader>lca', '<cmd>CocFix<cr>', { desc = "code action" })
-  end,
-  config = function()
+    vim.keymap.set('n', '<leader>la', '<Plug>(coc-fix-current)', { desc = "code action", silent = true, nowait = true })
+    vim.keymap.set('n', "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"',
+      { silent = true, nowait = true, expr = true })
+    vim.keymap.set('n', "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"',
+      { silent = true, nowait = true, expr = true })
     LM.api.complete = function()
-      vim.cmd([[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]])
+      vim.fn.eval("coc#refresh()")
     end
+    vim.keymap.set('i', '<cr>',
+      [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], {
+        silent = true,
+        noremap = true,
+        expr = true,
+        replace_keycodes = false,
+      })
     vim.keymap.set('i', '<TAB>',
-      'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', { expr = true })
+      'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', {
+        silent = true,
+        noremap = true,
+        expr = true,
+        replace_keycodes = false,
+      })
+    vim.keymap.set('i', '<S-TAB>', [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], {
+      silent = true,
+      noremap = true,
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.keymap.set('i', '<c-j>',
+      "<Plug>(coc-snippets-expand-jump)", {
+        silent = true,
+        noremap = true,
+        expr = true,
+        replace_keycodes = false,
+      })
     function _G.check_back_space()
       local col = vim.fn.col('.') - 1
       return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+    end
+
+    function _G.show_docs()
+      local cw = vim.fn.expand('<cword>')
+      if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+      elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+      else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+      end
     end
   end,
 }
