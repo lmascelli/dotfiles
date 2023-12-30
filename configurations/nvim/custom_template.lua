2f424e7f91168083d039d662f4b30ce789358edf
lmascelli <lmascelli@gmail.com>
2023-12-30 20:57:59 +0100

    neovide

--- configurations/nvim/custom_template.lua
+++ configurations/nvim/custom_template.lua
@@ -1,11 +1,39 @@
-LM.appearence.colorscheme = 'default'
+LM.appearence.colorscheme = 'gruvbox'
 
 LM.font.font_name = 'Cascadia Code'
 LM.font.font_size = '10'
+vim.opt.linespace = 3
 
 --------------------------------------------------------------------------------
 -- Make a plugins table
-local plugins_list = {
+
+local coc_list = false
+
+local plugins_list_native = {
+  'nvim-qt',
+  'which_key',
+  'telescope',
+  'treesitter',
+  'nvim-cmp',
+  'luasnip',
+  'lsp',
+  -- 'lsp_zero',
+  -- 'lsp_saga',
+  -- 'nvim-autopairs',
+  -- 'nvim-comment',
+  -- 'coc',
+  'colorschemes',
+  'nvim-tree',
+  'nvim-surround',
+  'zen-mode',
+  'tagbar',
+  'clangd',
+  -- 'rust',
+  'orgmode',
+  -- 'pets',
+}
+
+local plugins_list_coc = {
   'nvim-qt',
   'which_key',
   -- 'telescope',
@@ -19,15 +47,20 @@ local plugins_list = {
   -- 'nvim-comment',
   'coc',
   'colorschemes',
-  'nvim-tree',
+  -- 'nvim-tree',
   'nvim-surround',
   'zen-mode',
   'tagbar',
-  -- 'clangd',
+  'clangd',
   -- 'rust',
   'orgmode',
   -- 'pets',
 }
+
+local plugins_list = plugins_list_native
+if coc_list then
+  plugins_list = plugins_list_coc
+end
 
 for _, plugin in ipairs(plugins_list) do
   table.insert(LM.plugins.files_list, plugin)
--- configurations/nvim/lua/lm/plugins/neovide.lua
+++ configurations/nvim/lua/lm/plugins/neovide.lua
@@ -2,6 +2,9 @@ local font_name = LM.font.font_name or ''
 local font_size = LM.font.font_size or 10
 vim.g.neovide_remember_window_size = false
 vim.g.neovide_remember_window_position = false
+vim.g.neovide_refresh_rate = 30
+vim.g.neovide_scroll_animation_length = 0.1
+vim.g.neovide_scroll_animation_far_lines = 1
 
 local update_font = function()
   local ufont = font_name:gsub(' ', '\\ ')
@@ -18,5 +21,8 @@ end
 
 update_font()
 
-vim.keymap.set('n', '<leader>a+', '<cmd>lua LM.api.appearence.increase_scale()<cr>', { desc = 'Increase font' })
-vim.keymap.set('n', '<leader>a-', '<cmd>lua LM.api.appearence.decrease_scale()<cr>', { desc = 'Decrease font' })
+vim.keymap.set('n', '<leader>+', '<cmd>lua LM.api.appearence.increase_scale()<cr>', { desc = 'Increase font' })
+vim.keymap.set('n', '<leader>-', '<cmd>lua LM.api.appearence.decrease_scale()<cr>', { desc = 'Decrease font' })
+vim.keymap.set('n', '<f11>', function()
+  vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+end, { desc = 'Decrease font' })
