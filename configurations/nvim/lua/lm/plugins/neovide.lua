bd992fdab8d25d196b13b65c77340fa411aa6f31
lmascelli <lmascelli@gmail.com>
2024-06-16 12:19:09 +0200

    neovide update

--- configurations/nvim/lua/lm/plugins/neovide.lua
+++ configurations/nvim/lua/lm/plugins/neovide.lua
@@ -3,7 +3,9 @@ local font_size = LM.appearence.font.font_size or 10
 vim.g.neovide_remember_window_size = false
 vim.g.neovide_remember_window_position = false
 vim.g.neovide_refresh_rate = 30
-vim.g.neovide_scroll_animation_length = 0.1
+vim.g.neovide_scroll_animation_length = 0
+vim.g.neovide_cursor_animation_length = 0
+-- vim.g.neovide_scroll_animation_length = 0.1
 vim.g.neovide_scroll_animation_far_lines = 1
 
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
@@ -15,16 +17,20 @@ end
 
 LM.api.appearence.increase_scale = function()
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.15
+  vim.api.nvim__redraw({ win = vim.api.nvim_get_current_win(), valid = false })
 end
 
 LM.api.appearence.decrease_scale = function()
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / 1.15
+  vim.api.nvim__redraw({ win = vim.api.nvim_get_current_win(), valid = false })
 end
 
 update_font()
 
 vim.keymap.set('n', '<leader>+', '<cmd>lua LM.api.appearence.increase_scale()<cr>', { desc = 'Increase font' })
+vim.keymap.set('n', '<c-+>', '<cmd>lua LM.api.appearence.increase_scale()<cr>', { desc = 'Increase font' })
 vim.keymap.set('n', '<leader>-', '<cmd>lua LM.api.appearence.decrease_scale()<cr>', { desc = 'Decrease font' })
+vim.keymap.set('n', '<c-->', '<cmd>lua LM.api.appearence.decrease_scale()<cr>', { desc = 'Decrease font' })
 vim.keymap.set('n', '<f11>', function()
   vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
 end, { desc = 'decrease font' })
