9fd1b74697e917ac4b20cdffbeadf7d377d540ec
lmascelli <lmascelli@gmail.com>
2024-09-29 21:44:53 +0200

    emacs new.el

--- configurations/emacs/custom.el
+++ configurations/emacs/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi)))
+ '(custom-enabled-themes '(modus-vivendi))
+ '(package-selected-packages '(evil-collection evil rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ configurations/emacs/post-init.el
@@ -0,0 +1,11 @@
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :ensure t
+  :config
+  (evil-collection-init))
