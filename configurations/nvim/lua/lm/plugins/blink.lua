3f01aaf356577c3b66450291130aac00456f2fdd
lmascelli <lmascelli@gmail.com>
2024-12-21 19:53:50 +0100

    nvim blink.cmp super-tab keymap preset

--- configurations/nvim/lua/lm/plugins/blink.lua
+++ configurations/nvim/lua/lm/plugins/blink.lua
@@ -19,7 +19,7 @@ return {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
-      keymap = { preset = 'default' },
+      keymap = { preset = 'super-tab' },
 
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -42,8 +42,14 @@ return {
       -- experimental signature help support
       signature = { enabled = true }
     })
-  LM.lsp.capabilities = vim.tbl_extend("force", LM.lsp.capabilities, require('blink.cmp').get_lsp_capabilities())
+
+    LM.lsp.capabilities = vim.tbl_extend(
+      "force",
+      LM.lsp.capabilities,
+      require('blink.cmp').get_lsp_capabilities()
+    )
   end,
+
   -- allows extending the providers array elsewhere in your config
   -- without having to redefine it
   opts_extend = { "sources.default" },
