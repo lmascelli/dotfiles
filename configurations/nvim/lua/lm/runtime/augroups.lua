ed3dc07aaf204f893d69fb03166aed0a3b81432d
lmascelli <lmascelli@gmail.com>
2025-07-09 16:43:07 +0200

    nvim cleaned a bit runtime configuration

--- configurations/nvim/lua/lm/runtime/augroups.lua
+++ configurations/nvim/lua/lm/runtime/augroups.lua
@@ -5,13 +5,5 @@ LM.augroups = {
   chdir = vim.api.nvim_create_augroup("LMChDir", {
     clear = true,
   }),
-  gui = vim.api.nvim_create_augroup("LMGui", {
-    clear = true,
-  }),
-  lsp_menu = vim.api.nvim_create_augroup("LMLspMenu", {
-    clear = true,
-  }),
-  lsp = vim.api.nvim_create_augroup("LMLspStart", {
-    clear = true
-  }),
+  lspconfig = vim.api.nvim_create_augroup('UserLspConfig', {}),
 }
--- configurations/nvim/lua/lm/runtime/lsp/init.lua
+++ configurations/nvim/lua/lm/runtime/lsp/init.lua
@@ -1,18 +1,5 @@
--- floating windows
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  vim.lsp.handlers.hover, {
-    border = LM.appearence.floating_border_style,
-  }
-)
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-  vim.lsp.handlers.signature_help, {
-    border = LM.appearence.floating_border_style,
-  }
-)
-
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  group = LM.augroups.lspconfig,
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
@@ -29,75 +16,31 @@ vim.api.nvim_create_autocmd('FileType', {
   end
 })
 
-local function make_menu(bufnr)
-  vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-      vim.cmd([[
-            :amenu 10 PopUp.hover <cmd>lua vim.lsp.buf.hover()<cr>
-            ]])
-    end,
-    group = LM.augroups.lsp_menu,
-    buffer = bufnr,
-  })
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-  vim.api.nvim_create_autocmd("BufLeave", {
-    callback = function()
-      vim.cmd([[
-            :silent! unmenu PopUp.hover
-            ]])
-    end,
-    group = LM.augroups.lsp_menu,
-    buffer = bufnr,
-  })
-end
+capabilities.window = capabilities.window or {}
+capabilities.window.workDoneProgress = true
 
--- Check if LSP is enabled in the current buffer
-local function is_enabled()
-  local clients = vim.lsp.get_clients({
-    bufnr = vim.api.nvim_get_current_buf()
-  })
-  if next(clients) ~= nil then
-    return true
-  else
-    return false
-  end
-end
-
-local function configure(language)
-  local config = LM.languages[language]
-  if config ~= nil then
-    local config = config.lsp_config
-    if vim.fn.executable(config.cmd[1]) ~= 0 then
-      vim.api.nvim_create_autocmd("FileType", {
-        group = LM.augroups.lsp,
-        pattern = config.filetypes,
-        callback = function(ev)
-          vim.lsp.start(config, {
-            bufnr = ev.buf
-          })
-        end
-      })
-    else
-      vim.print("LM.lsp.configure: " .. language .. " not present in configurations")
-    end
-  end
-
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("LMLspAttach", { clear = false }),
-    callback = function(ev)
-      vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, { autotrigger = true })
-      vim.bo[ev.buf].formatexpr = 'v:lua.vim.lsp.formatexpr()'
-    end
-  })
-end
-
+-- https://microsoft.github.io/language-server-protocol/specifications/specification-3-17/#completionClientCapabilities
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits",
+  },
+}
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
 
 LM.lsp = {
-  capabilities = require 'lm.runtime.lsp.capability',
+  capabilities = capabilities,
   on_attach = require 'lm.runtime.lsp.on_attach',
-  configure = configure,
   configs = require 'lm.runtime.lsp.configs',
   c_query_driver = 'g++',
-  is_enabled = is_enabled,
   servers = {},
 }
