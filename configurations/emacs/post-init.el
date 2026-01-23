ee6996559d01c4c9c65f362ac90210649c288415
lmascelli <lmascelli@gmail.com>
2026-01-23 11:32:39 +0100

    emacs old literal config cleanup

--- configurations/emacs/post-init.el
+++ configurations/emacs/post-init.el
@@ -5,17 +5,9 @@
 (use-package lm-enhanced-eshell :after eshell)
 (use-package lm-terminal-here)
 
-(unless (display-graphic-p)
-  (use-package xclip
-	:ensure t
-	:config
-	(xclip-mode)))
-
-
-(require 'lm-company)
 (require 'lm-ultra-scroll)
 
-;; (set-frame-size nil 80 40)
 (menu-bar-mode 1)
 
 (setq default-directory "C:/Users/leona/")
+(setq package-check-signature nil)
